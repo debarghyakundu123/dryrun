ae42e11d3a612a802f7e77e4bee58f7526c704bd
DEBARGHYA KUNDU <129304569+debarghyakundu123@users.noreply.github.com>
2025-02-15 06:54:43 +0530

    1.py

--- 1.py
+++ 1.py
@@ -7,22 +7,19 @@ st.write("Welcome to the Education Platform. Select a course below:")
 
 courses = {
     "MAIN CHENNAL": "https://t.me/+XuIEiecTujMxODNl",
-    "SSC BANKING RAILWAY ": "https://t.me/+6rX0Xxg3FtBmYTE1",
+    "SSC BANKING RAILWAY": "https://t.me/+6rX0Xxg3FtBmYTE1",
     "UPSC": "https://t.me/+vBpYFICH6HczY2M9",
-    "VIP MEMBERS ": "https://t.me/+qM06_7HP4PdkNzk1",
+    "VIP MEMBERS": "https://t.me/+qM06_7HP4PdkNzk1",
     "INFINITE COURSES": "https://t.me/+PTnRqzn8PfczM2I1",
     "NEW COURSES 2025": "https://t.me/+FMHvMLefobowZjRl",
-    "ENTERTAINMENT ": "https://t.me/+-2ca3mBA87w5YTll"
-
+    "ENTERTAINMENT": "https://t.me/+-2ca3mBA87w5YTll"
 }
+
 st.image("home.jpg", caption="Welcome to the Education Platform", use_column_width=True)
 
-
 for course, link in courses.items():
-    st.markdown(f"- {course}")
-
+    st.markdown(f"- [{course}]({link})")
 
 st.write("### 📢 Connect with us on Social Media")
 st.markdown("[📢 Join our Telegram](https://t.me/+XuIEiecTujMxODNl)")
-st.markdown("[ Follow us on Whatsapp](https://whatsapp.com/channel/0029Vb25egi7oQhbkaBNse2o)")
-st.markdown
+st.markdown("[💬 Follow us on Whatsapp](https://whatsapp.com/channel/0029Vb25egi7oQhbkaBNse2o)")
